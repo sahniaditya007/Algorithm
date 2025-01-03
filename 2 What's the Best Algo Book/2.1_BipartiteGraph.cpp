112fa86842c6eafe569d145f1fb61f5daeceaef1
Aditya Sahni <95126292+sahniaditya007@users.noreply.github.com>
2025-01-03 17:01:34 +0530

    03-01-2025

---
+++ 2 What's the Best Algo Book/2.1_BipartiteGraph.cpp
@@ -0,0 +1,5 @@
+//https://www.geeksforgeeks.org/problems/bipartite-graph/1
+//color the graph with 2 colors such that no adjacent (neighour) node have the same color
+//linear graph are always bipartite graph also even cycle graph
+//odd cycle wale graphs bipartite nahi ho sakte hai
+//issme bfs use karenge
