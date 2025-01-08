60e117283f0a3cbbf31caa16666adf2469a9ac3d
Aditya Sahni <95126292+sahniaditya007@users.noreply.github.com>
2025-01-08 14:57:02 +0530

    08-01-2022

--- 2 What's the Best Algo Book/2.2_Bellman-Ford.cpp
+++ 2 What's the Best Algo Book/2.2_Bellman-Ford.cpp
@@ -1,6 +1,9 @@
+//https://www.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/1
 //used to find the shortest path
 //also considers negative edges unlike Dijaktra
 //only applicalble in directed graph
 //helps to dected negative cycle
 //Reach all the edges (n-1) times sequentially
 //Reach = cy(dist[u] + wt < dist[v])
+//dist[v] = dist[u] + w
+// ye samaj nahi aa raha hai abhi baad mei aayenge idha
